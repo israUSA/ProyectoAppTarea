7c101405e8c1287b36dc2a36c96e5fb3a50413d6
Stalyn Asitimbay <stalynasi08@gmail.com>
2023-06-14 23:23:36 -0500

    se agregeo la funcion Buscar2

--- app/src/main/java/com/example/proyectoapptarea/activity_mostrar_tareas.java
+++ app/src/main/java/com/example/proyectoapptarea/activity_mostrar_tareas.java
@@ -23,101 +23,96 @@ import com.example.proyectoapptarea.entidades.listaTareas;
 import java.util.ArrayList;
 import java.util.List;
 
-    public class activity_mostrar_tareas extends AppCompatActivity {
-        List<listaTareas> elements;
-        listaAdaptador listAdapter;
-        EditText editTextSearch;
-        Button buttonSearch;
-        RecyclerView cardTareas;
+public class activity_mostrar_tareas extends AppCompatActivity {
+    List<listaTareas> elements;
+    listaAdaptador listAdapter;
+    EditText editTextSearch;
+    Button buttonSearch;
+    RecyclerView cardTareas;
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            editTextSearch = findViewById(R.id.editTextSearch);
-            buttonSearch = findViewById(R.id.buttonSearch);
-            cardTareas = findViewById(R.id.cardTareas);
-            setContentView(R.layout.activity_mostrar_tareas);
-            init();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
 
-            buttonSearch.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-/*                    String searchText = editTextSearch.getText().toString();*/
-                    Log.d("HOLA", "TEXTO BUSC: ");
-                }
-            });
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mostrar_tareas);
+        editTextSearch = findViewById(R.id.editTextSearch);
+        buttonSearch = findViewById(R.id.buttonSearch);
+        cardTareas = findViewById(R.id.cardTareas);
 
-
-        }
-
-        @SuppressLint("Range")
-        private void obtenerDatosDeLaBaseDeDatos() {
-            BDTareaApp bdTareaApp = new BDTareaApp(this);
-            SQLiteDatabase db = bdTareaApp.getWritableDatabase();
-
-            if (db != null) {
-                elements = new ArrayList<>();
-//                TextView tituloTmp = findViewById(R.id.ViuwTitulo);
-//                TextView descripcionTmp = findViewById(R.id.ViuwDescripcion);
-//                TextView FechaLimiteTmp = findViewById(R.id.ViuwFechaLimite);
-
-                Cursor ct = db.rawQuery("SELECT titulo_tarea, descripcion, fechaVencimiento FROM Tarea", null);
-                if (ct.moveToFirst()) {
-                    do {
-                        String titulo = ct.getString(ct.getColumnIndex("titulo_tarea"));
-                        String descripcion = ct.getString(ct.getColumnIndex("descripcion"));
-                        String fechaLimite = ct.getString(ct.getColumnIndex("fechaVencimiento"));
-                        listaTareas tarea = new listaTareas(titulo, descripcion, fechaLimite);
-
-                        elements.add(tarea);
-                    } while (ct.moveToNext());
-                }
-                ct.close();
-/*                db.close();*/
+        buttonSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                obtenerDatosDeLaBaseDeDatosBusqueda();
+                listAdapter.notifyDataSetChanged();
             }
-        }
+        });
 
-/*        @SuppressLint("Range")
-        private void obtenerDatosDeLaBaseDeDatosBusqueda() {
-            BDTareaApp bdTareaApp = new BDTareaApp(this);
-            SQLiteDatabase db = bdTareaApp.getWritableDatabase();
+        init();
+    }
 
-            editTextSearch = findViewById(R.id.editTextSearch);
-            String nombre = editTextSearch.getText().toString();
+    @SuppressLint("Range")
+    private void obtenerDatosDeLaBaseDeDatos() {
+        BDTareaApp bdTareaApp = new BDTareaApp(this);
+        SQLiteDatabase db = bdTareaApp.getWritableDatabase();
 
-            if (db != null) {
-                elements = new ArrayList<>();
-//                TextView tituloTmp = findViewById(R.id.ViuwTitulo);
-//                TextView descripcionTmp = findViewById(R.id.ViuwDescripcion);
-//                TextView FechaLimiteTmp = findViewById(R.id.ViuwFechaLimite);
+        if (db != null) {
+            elements = new ArrayList<>();
 
-                Cursor ct = db.rawQuery("SELECT * FROM Tarea WHERE titulo_tarea LIKE '%" + nombre + "%'");
-                if (ct.moveToFirst()) {
-                    do {
-                        String titulo = ct.getString(ct.getColumnIndex("titulo_tarea"));
-                        String descripcion = ct.getString(ct.getColumnIndex("descripcion"));
-                        String fechaLimite = ct.getString(ct.getColumnIndex("fechaVencimiento"));
-                        listaTareas tarea = new listaTareas(titulo, descripcion, fechaLimite);
+            Cursor ct = db.rawQuery("SELECT titulo_tarea, descripcion, fechaVencimiento FROM Tarea", null);
+            if (ct.moveToFirst()) {
+                do {
+                    String titulo = ct.getString(ct.getColumnIndex("titulo_tarea"));
+                    String descripcion = ct.getString(ct.getColumnIndex("descripcion"));
+                    String fechaLimite = ct.getString(ct.getColumnIndex("fechaVencimiento"));
+                    listaTareas tarea = new listaTareas(titulo, descripcion, fechaLimite);
 
-                        elements.add(tarea);
-                    } while (ct.moveToNext());
-                }
-                ct.close();
-                //db.close();
+                    elements.add(tarea);
+                } while (ct.moveToNext());
             }
-        }*/
-
-        public void init() {
-            obtenerDatosDeLaBaseDeDatos();
-            listAdapter = new listaAdaptador(elements, this);
-            RecyclerView recyclerView = findViewById(R.id.cardTareas);
-            recyclerView.setHasFixedSize(true);
-            recyclerView.setLayoutManager(new LinearLayoutManager(this));
-            recyclerView.setAdapter(listAdapter);
-            listAdapter.notifyDataSetChanged();
+            ct.close();
+            db.close();
         }
     }
 
+    @SuppressLint("Range")
+    private void obtenerDatosDeLaBaseDeDatosBusqueda() {
+        BDTareaApp bdTareaApp = new BDTareaApp(this);
+        SQLiteDatabase db = bdTareaApp.getWritableDatabase();
+
+        editTextSearch = findViewById(R.id.editTextSearch);
+        String nombre = editTextSearch.getText().toString();
+
+        if (db != null) {
+            String consulta = "SELECT * FROM Tarea WHERE LOWER(titulo_tarea) LIKE '%" + nombre + "%'";
+            Cursor ct = db.rawQuery(consulta, null);
+
+            elements.clear(); // Eliminar los elementos anteriores en lugar de crear una nueva instancia
+
+            if (ct.moveToFirst()) {
+                do {
+                    String titulo = ct.getString(ct.getColumnIndex("titulo_tarea"));
+                    String descripcion = ct.getString(ct.getColumnIndex("descripcion"));
+                    String fechaLimite = ct.getString(ct.getColumnIndex("fechaVencimiento"));
+                    listaTareas tarea = new listaTareas(titulo, descripcion, fechaLimite);
+
+                    elements.add(tarea);
+                } while (ct.moveToNext());
+            }
+            ct.close();
+            db.close();
+        }
+    }
+
+    public void init() {
+        obtenerDatosDeLaBaseDeDatos();
+        listAdapter = new listaAdaptador(elements, this);
+        RecyclerView recyclerView = findViewById(R.id.cardTareas);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(listAdapter);
+        listAdapter.notifyDataSetChanged();
+    }
+}
 
 
 
