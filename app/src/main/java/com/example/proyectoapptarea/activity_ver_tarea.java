aa3e0c92132ced731becacf1a32af294b8fba053
Israel Sellan <leonardosellan20@gmail.com>
2023-06-22 01:51:11 -0500

    menu recordatorios/notificaciones

--- app/src/main/java/com/example/proyectoapptarea/activity_ver_tarea.java
+++ app/src/main/java/com/example/proyectoapptarea/activity_ver_tarea.java
@@ -9,6 +9,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.InputType;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
@@ -116,6 +118,24 @@ public class activity_ver_tarea extends AppCompatActivity {
     private void lista(){
         Intent intent = new Intent(this, activity_mostrar_tareas.class);
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_notificaciones, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+
+        if (itemId == R.id.it_notificaciones) {
+            Log.d("Menu", "Notificaciones");
+            return true;
+
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
     }
 
 }
